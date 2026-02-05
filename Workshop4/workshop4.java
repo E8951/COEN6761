55b8ea5174732846362275904d5eb6aedf8447a0
E8951 <aliawada438@gmail.com>
2026-02-05 14:34:20 -0800

    Correct DFS color handling and traversal logic
    Fix incorrect handling of visited nodes and neighbor traversal logic.

--- Workshop4/workshop4.java
+++ Workshop4/workshop4.java
@@ -10,20 +10,14 @@ class workshop4 {
         Foo gColor = new Foo() {
             @Override
             public boolean dfs_color(int node, int color) {
-                // BUG #1: Incorrectly handling already visited nodes.
-                // Instead of returning false if colors mismatch, we only check if it equals
-                // current.
-                // This could lead to infinite recursion or incorrect true results.
                 if (visited[node] != 0) {
-                    return true; // Should be: return (visited[node] == color);
+                    return true;
                 }
 
                 visited[node] = color;
 
                 for (int i = 0; i < graph[node].length; i++) {
                     int neighbor = graph[node][i];
-                    // BUG #2: Logic error in neighbor traversal.
-                    // Using "color" instead of "-color" for the neighbor.
                     boolean res = dfs_color(neighbor, color);
                     if (!res)
                         return false;
